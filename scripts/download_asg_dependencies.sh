2c939bab47182b9f9ad95c49ca6bbf01ff282de9
Andrew Zong <a.zong@f5.com>
2019-03-23 15:10:36 +0800

    Add starting scripts and docker-compose settting for DO and ASG.

---
+++ scripts/download_asg_dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# TODO: make /var/tmp/ASGExtensions and filelist configurable.
+filelist="
+https://github.com/jgruber/TrustedDevices/releases/download/1.1.1/TrustedDevices-1.1.1-0001.noarch.rpm
+https://github.com/jgruber/TrustedProxy/releases/download/1.0.1/TrustedProxy-1.0.1-0002.noarch.rpm
+https://github.com/jgruber/TrustedExtensions/releases/download/1.0.1/TrustedExtensions-1.0.1-0001.noarch.rpm
+https://github.com/jgruber/TrustedASMPolicies/releases/download/1.0.5/TrustedASMPolicies-1.0.5-0004.noarch.rpm
+"
+mkdir -p /var/tmp/ASGExtensions
+
+(
+    cd /var/tmp/ASGExtensions
+    for n in $filelist; do
+        filebase=`basename $n`
+        if [ ! -f $filebase ]; then 
+            wget $n
+        fi
+    done
+)
+
