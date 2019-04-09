eb393fb38f9ad89af2e11ec36e3b2defc0e7cae0
pei <j.pei@f5.com>
2019-04-09 13:32:44 +0800

    modify based on the review suggestions

--- app/waf/src/models/endpointpolicy.model.ts
+++ app/waf/src/models/endpointpolicy.model.ts
@@ -1,24 +1,9 @@
-import {model, property, hasMany} from '@loopback/repository';
+import {model, hasMany} from '@loopback/repository';
 import {CommonEntity} from '.';
 import {Rule} from '../models';
 
 @model()
 export class Endpointpolicy extends CommonEntity {
-  @property({
-    type: 'array',
-    itemType: 'string',
-    required: false,
-    default: [],
-    schema: {
-      create: true,
-      update: true,
-      response: true,
-      example: [
-        '23442d6a-4d82-40ee-8d08-243750dbc191',
-        'aaaabbda-4d82-40ee-8d08-243750dbc192',
-      ],
-    },
-  })
   @hasMany(() => Rule, {keyTo: 'endpointpolicyId'})
   rules?: Rule[];
 
