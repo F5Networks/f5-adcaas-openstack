8a3391278ae786b70f6687cb2555b9fd74a3169c
Peter <peter.zhang@f5.com>
2019-05-08 11:01:39 +0800

    add multi-tenancy for pool and member (one to many relationship)

--- app/waf/src/controllers/pool.controller.ts
+++ app/waf/src/controllers/pool.controller.ts
@@ -8,10 +8,14 @@ import {
   del,
   requestBody,
   HttpErrors,
+  RequestContext,
+  RestBindings,
 } from '@loopback/rest';
 import {Pool, Member} from '../models';
 import {PoolRepository, MemberRepository} from '../repositories';
 import {Schema, Response, CollectionResponse} from '.';
+import {BaseController} from './base.controller';
+import {inject} from '@loopback/core';
 
 const prefix = '/adcaas/v1';
 const createDesc: string = 'Pool resource that need to be created';
@@ -20,13 +24,17 @@ const createMemberDesc: string = 'Member resource that need to be created';
 const updateMemberDesc: string =
   'Member resource properties that need to be updated';
 
-export class PoolController {
+export class PoolController extends BaseController {
   constructor(
     @repository(PoolRepository)
     public poolRepository: PoolRepository,
     @repository(MemberRepository)
     public memberRepository: MemberRepository,
-  ) {}
+    @inject(RestBindings.Http.CONTEXT)
+    protected reqCxt: RequestContext,
+  ) {
+    super(reqCxt);
+  }
 
   @post(prefix + '/pools', {
     responses: {
@@ -58,7 +66,9 @@ export class PoolController {
   async find(
     @param.query.object('filter', getFilterSchemaFor(Pool)) filter?: Filter,
   ): Promise<CollectionResponse> {
-    const data = await this.poolRepository.find(filter);
+    const data = await this.poolRepository.find(filter, {
+      tenantId: await this.tenantId,
+    });
     return new CollectionResponse(Pool, data);
   }
 
@@ -72,7 +82,9 @@ export class PoolController {
     @param(Schema.pathParameter('poolId', 'Pool resource ID'))
     id: string,
   ): Promise<Response> {
-    const data = await this.poolRepository.findById(id);
+    const data = await this.poolRepository.findById(id, undefined, {
+      tenantId: await this.tenantId,
+    });
     return new Response(Pool, data);
   }
 
@@ -88,7 +100,9 @@ export class PoolController {
     @requestBody(Schema.updateRequest(Pool, updateDesc))
     pool: Partial<Pool>,
   ): Promise<void> {
-    await this.poolRepository.updateById(id, pool);
+    await this.poolRepository.updateById(id, pool, {
+      tenantId: await this.tenantId,
+    });
   }
 
   @del(prefix + '/pools/{poolId}', {
@@ -101,7 +115,9 @@ export class PoolController {
     @param(Schema.pathParameter('poolId', 'Pool resource ID'))
     id: string,
   ): Promise<void> {
-    await this.poolRepository.deleteById(id);
+    await this.poolRepository.deleteById(id, {
+      tenantId: await this.tenantId,
+    });
   }
 
   /**
@@ -123,6 +139,7 @@ export class PoolController {
     @requestBody(Schema.createRequest(Member, createMemberDesc))
     member: Partial<Member>,
   ): Promise<Response> {
+    // TODO: tenantId needs to be assigned by code, but not manually.
     const data = await this.poolRepository.members(pool_id).create(member);
     return new Response(Member, data);
   }
@@ -143,7 +160,8 @@ export class PoolController {
   ): Promise<CollectionResponse> {
     const data = await this.poolRepository
       .members(pool_id)
-      .find({where: {id: member_id}});
+      .find({where: {and: [{id: member_id}, {tenantId: await this.tenantId}]}});
+
     return new CollectionResponse(Member, data);
   }
 
@@ -159,7 +177,9 @@ export class PoolController {
     @param(Schema.pathParameter('poolId', 'Pool resource ID'))
     pool_id: string,
   ): Promise<CollectionResponse> {
-    const data = await this.poolRepository.members(pool_id).find();
+    const data = await this.poolRepository
+      .members(pool_id)
+      .find({where: {tenantId: await this.tenantId}});
     return new CollectionResponse(Member, data);
   }
 
@@ -176,7 +196,9 @@ export class PoolController {
     @param(Schema.pathParameter('memberId', 'Member resource ID'))
     member_id: string,
   ): Promise<void> {
-    await this.poolRepository.members(pool_id).delete({id: member_id});
+    await this.poolRepository
+      .members(pool_id)
+      .delete({and: [{id: member_id}, {tenantId: await this.tenantId}]});
   }
 
   @patch(prefix + '/pools/{poolId}/members/{memberId}', {
@@ -195,6 +217,8 @@ export class PoolController {
     @requestBody(Schema.createRequest(Member, updateMemberDesc))
     member: Partial<Member>,
   ): Promise<void> {
-    await this.poolRepository.members(pool_id).patch(member, {id: member_id});
+    await this.poolRepository
+      .members(pool_id)
+      .patch(member, {and: [{id: member_id}, {tenantId: await this.tenantId}]});
   }
 }
--- app/waf/test/acceptance/pool.contoller.acceptance.ts
+++ app/waf/test/acceptance/pool.contoller.acceptance.ts
@@ -1,27 +1,77 @@
 import {Client, expect, toJSON} from '@loopback/testlab';
 import {WafApplication} from '../..';
-import {setupApplication, teardownApplication} from '../helpers/test-helper';
+import {
+  setupApplication,
+  teardownApplication,
+  TestingApplication,
+  setupRestAppAndClient,
+  RestApplicationPort,
+  teardownRestAppAndClient,
+} from '../helpers/test-helper';
 import {
   givenEmptyDatabase,
   givenPoolData,
   createPoolObject,
 } from '../helpers/database.helpers';
+import {
+  ShouldResponseWith,
+  MockKeyStoneController,
+  ExpectedData,
+} from '../fixtures/controllers/mocks/mock.openstack.controller';
 
 describe('PoolController', () => {
   let wafapp: WafApplication;
   let client: Client;
+  let mockKeystoneApp: TestingApplication;
 
   const prefix = '/adcaas/v1';
 
+  let envs: {[key: string]: string} = {
+    OS_AUTH_URL: 'http://localhost:35357/v2.0',
+    OS_USERNAME: 'wafaas',
+    OS_PASSWORD: '91153c85b8dd4147',
+    OS_TENANT_ID: '32b8bef6100e4cb0a984a7c1f9027802',
+    OS_DOMAIN_NAME: 'Default',
+    OS_REGION_NAME: 'RegionOne',
+    OS_AVAILABLE_ZONE: 'nova',
+  };
+
   before('setupApplication', async () => {
+    mockKeystoneApp = await (async () => {
+      let {restApp} = await setupRestAppAndClient(
+        RestApplicationPort.IdentityAdmin,
+        MockKeyStoneController,
+      );
+      return restApp;
+    })();
+
     ({wafapp, client} = await setupApplication());
+    ShouldResponseWith({});
+    setupEnvs();
   });
+
   beforeEach('Empty database', async () => {
     await givenEmptyDatabase(wafapp);
   });
 
+  let setupEnvs = async () => {
+    process.env.PRODUCT_RELEASE = '1';
+    for (let env of Object.keys(envs)) {
+      process.env[env] = envs[env];
+    }
+  };
+
+  let teardownEnvs = async () => {
+    delete process.env['PRODUCT_RELEASE'];
+    for (let env of Object.keys(envs)) {
+      delete process.env[env];
+    }
+  };
+
   after(async () => {
     await teardownApplication(wafapp);
+    teardownRestAppAndClient(mockKeystoneApp);
+    teardownEnvs();
   });
 
   it('post ' + prefix + '/pools', async () => {
@@ -29,6 +79,8 @@ describe('PoolController', () => {
 
     const response = await client
       .post(prefix + '/pools')
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
       .send(pool)
       .expect(200);
     expect(response.body.pool.id)
@@ -40,14 +92,22 @@ describe('PoolController', () => {
   it('get ' + prefix + '/pools', async () => {
     const pool = await givenPoolData(wafapp);
 
-    const response = await client.get(prefix + '/pools').expect(200);
+    const response = await client
+      .get(prefix + '/pools')
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
+      .expect(200);
     expect(toJSON(pool)).to.containDeep(response.body.pools[0]);
   });
 
   it('get ' + prefix + '/pools/{id}', async () => {
     const pool = await givenPoolData(wafapp);
 
-    const response = await client.get(prefix + `/pools/${pool.id}`).expect(200);
+    const response = await client
+      .get(prefix + `/pools/${pool.id}`)
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
+      .expect(200);
 
     expect(response.body.pool.id).equal(pool.id);
   });
@@ -59,17 +119,31 @@ describe('PoolController', () => {
 
     await client
       .patch(prefix + `/pools/${pool.id}`)
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
       .send(pool)
       .expect(204);
 
-    await client.get(prefix + `/pools/${pool.id}`).expect(200);
+    await client
+      .get(prefix + `/pools/${pool.id}`)
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
+      .expect(200);
   });
 
   it('delete ' + prefix + '/pools/{id}', async () => {
     const pool = await givenPoolData(wafapp);
 
-    await client.del(prefix + `/pools/${pool.id}`).expect(204);
+    await client
+      .del(prefix + `/pools/${pool.id}`)
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
+      .expect(204);
 
-    await client.get(prefix + `/pools/${pool.id}`).expect(404);
+    await client
+      .get(prefix + `/pools/${pool.id}`)
+      .set('X-Auth-Token', ExpectedData.userToken)
+      .set('tenant-id', ExpectedData.tenantId)
+      .expect(404);
   });
 });
